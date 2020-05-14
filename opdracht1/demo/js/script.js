3c5039f0a49356ba0b04214845c8fd6a50581569
lvs222 <51123194+lvs222@users.noreply.github.com>
2020-05-14 12:48:57 +0200

    oplevering 14 mei 2020

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -5,10 +5,75 @@
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
 
-var button = document.querySelector("button");
-var element = document.querySelector("li");
+//dit zijn de variabelen voor het maken van favorieten //
 
-button.addEventListener("click", function() {
-  element.classList.toggle("yay");
+var buttonIronMan = document.querySelector("button.ironman");
+var favoIronMan = document.querySelector("li.ironmanfavo");
+
+var buttonTheProposal = document.querySelector("button.theproposal");
+var favoTheProposal = document.querySelector("li.theproposalfavo");
+
+var buttonMamaMia = document.querySelector("button.mamamia");
+var favoMamaMia = document.querySelector("li.mamamiafavo");
+
+var buttonCaptainAmerica = document.querySelector("button.captainamerica");
+var favoCaptainAmerica = document.querySelector("li.captainamericafavo");
+
+//dit zijn de variabelen voor het niet-favorieten//
+
+var buttonIronManniet = document.querySelector("button.ironmangeen");
+var unfavoIronMan = document.querySelector("li.ironmanfavo");
+
+var buttonTheProposalniet = document.querySelector("button.theproposalgeen");
+var unfavoTheProposal = document.querySelector("li.theproposalfavo");
+
+var buttonMamaMianiet = document.querySelector("button.mamamiageen");
+var unfavoMamaMia = document.querySelector("li.mamamiafavo");
+
+var buttonCaptainAmericaniet = document.querySelector("button.captainamericageen");
+var unfavoCaptainAmerica = document.querySelector("li.captainamericafavo");
+
+//dit zijn de acties voor het maken van een favoriet//
+
+
+buttonIronMan.addEventListener("click", function() {
+    favoIronMan.classList.toggle("yay");
+    
+});
+
+buttonTheProposal.addEventListener("click", function() {
+    favoTheProposal.classList.toggle("yay");
+});
+
+
+buttonMamaMia.addEventListener("click", function() {
+    favoMamaMia.classList.toggle("yay");
+});
+
+buttonCaptainAmerica.addEventListener("click", function() {
+    favoCaptainAmerica.classList.toggle("yay");
+});
+
+
+//dit zijn de acties voor het niet meer favoriet maken//
+
+buttonIronManniet.addEventListener("click", function() {
+    unfavoIronMan.classList.toggle("yay");
+    
+});
+
+buttonTheProposalniet.addEventListener("click", function() {
+    unfavoTheProposal.classList.toggle("yay");
+    
+});
+
+buttonMamaMianiet.addEventListener("click", function() {
+    unfavoMamaMia.classList.toggle("yay");
+    
+});
+
+buttonCaptainAmericaniet.addEventListener("click", function() {
+    unfavoCaptainAmerica.classList.toggle("yay");
+    
 });
 
