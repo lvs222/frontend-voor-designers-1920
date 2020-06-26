646ea006fdecaa960fcf61de587502fe945e5f42
lvs222 <51123194+lvs222@users.noreply.github.com>
2020-06-26 10:11:48 +0200

    aanpassingen 26 juni

--- herkansing/js/script.js
+++ herkansing/js/script.js
@@ -1,26 +1,9 @@
 //hier maak ik de variabelen aan die ik in de functies ga gebruiken.
 var header = document.querySelector('header');
 var section = document.querySelector('section');
-var buttonVideo = document.getElementById("buttonvideo");
-var buttonPhoto = document.getElementById("buttonphoto");
-var showVideo = document.querySelector("div.videoelement");
-var hidePhoto = document.querySelector(".photo");
+var count = 0;
 
 
-//dit zijn de functies waarmee ik de tekst op de button laat veranderen, en waar ik de video en foto wel/niet laat zien. Dit gebeurt met een toggle
-buttonVideo.addEventListener("click", function () {
-
-    buttonVideo.classList.toggle("remove");
-
-});
-
-buttonPhoto.addEventListener("click", function () {
-    showVideo.classList.toggle("showvideo");
-
-    hidePhoto.classList.toggle("hidephoto");
-
-    buttonPhoto.classList.toggle("remove");
-});
 
 //het volgende stuk gaat over de json data 
 //variabele met de url naar de json data // 
@@ -47,10 +30,10 @@ request.onload = function () {
 }
 
 function showPhotos(jsonObj) {
-    console.log("function showPhotos", jsonObj);
+        console.log("function showPhotos", jsonObj);
 
-
-    for (let i = 0; i < jsonObj.length; i++) {
+  
+    for (let i = 0; i < 10; i++) {
 
         //html elementen aanmaken
         var myArticle = document.createElement('article');
@@ -58,27 +41,90 @@ function showPhotos(jsonObj) {
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var afbeelding = document.createElement('img');
-        var button = document.createElement('button');
+//        var video = document.createElement('video');
         
 
+
         myH2.textContent = jsonObj[i].title;
+
 
         afbeelding.src = jsonObj[i].thumbnail;
 
         myPara1.textContent = jsonObj[i].competition.name;
         
-        button.textContent = jsonObj[i].competition.name;
-        
+//        video.src = jsonObj[i].videos.embed;
+
+
 
         //html elementen genest
         myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(afbeelding);
-        myArticle.appendChild(button);
+//        myArticle.appendChild(video);
+        
+        
 
         //nieuwe elementen aan de section in de html toegevoegt// 
 
         section.appendChild(myArticle);
+        
+    }
+
+
+}
+
+
+// werking van de carousel 
+
+// variabelen voor de  terug en verder button 
+var buttonTerug = document.querySelector('#terugButton');
+var buttonVerder = document.querySelector('#verderButton');
+
+// variabele voor de section waar de thumbnails inzitten
+var footballCarousel = document.querySelector('section');
+
+// Buttons onclick
+
+// event listeners toevoegen aan de terug en verder buttons
+buttonTerug.addEventListener('click', navigatieTerug);
+buttonVerder.addEventListener('click', navigatieVerder);
+
+// functie voor wat er gebeurt als je op de terugButton klikt
+function navigatieTerug() {
+    count += 300;
+    // section met img transformeert elke keer +300 px
+    footballCarousel.style.transform = `translateX(${count}px)`;
+}
+
+// functie voor wat er gebeurt als je op de verderButton klikt
+function navigatieVerder() {
+    count -= 300;
+    // section met img transformeert elke keer -300 px
+    footballCarousel.style.transform = `translateX(${count}px)`;
+}
+
+
+//Keypress navigatie
+document.addEventListener('keydown', pressTerug);
+document.addEventListener('keydown', pressVerder);
+
+// functie aanmaken voor wat er gebeurt als je op de > knop op je toetsenbord drukt
+function pressTerug() {
+    if (event.keyCode == 39) {
+        count -= 300;
+        // section met img transformeert elke keer +300 px
+        footballCarousel.style.transform = `translateX(${count}px)`;
     }
 }
+
+// functie aanmaken voor wat er gebeurt als je op de < knop op je toetsenbord drukt
+function pressVerder() {
+    if (event.keyCode == 37) {
+        count += 300;
+        // section met transformeert elke keer +300 px
+        footballCarousel.style.transform = `translateX(${count}px)`;
+    }
+}
+
+
